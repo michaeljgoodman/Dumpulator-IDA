701f903b614df2027f4778cfa22680d84f397976
michaelgoodman-cr <michael.goodman@controlrisks.com>
2021-12-16 23:09:25 +0000

    added beta dynamic functionality

--- dumpulator.py
+++ dumpulator.py
@@ -28,13 +28,150 @@ def global_settings():
     global DUMP_FILE
     DUMP_FILE = ida_kernwin.ask_file(1, "*", "Enter the name of dump file:")
     print("You have selected " + DUMP_FILE)
+    global dp
+    dp = Dumpulator(DUMP_FILE)
     return 
 
 
 
+
 #--------------------------------------------------------------------------
-# dumpulate functions
+# Number of Args Form
 #--------------------------------------------------------------------------
+class arg_num_select_t(ida_kernwin.Form):
+    """Simple form to select how many args to initialise"""
+    def __init__(self):
+        self.__n = 0
+        F = ida_kernwin.Form
+        F.__init__(self,
+r"""BUTTON YES* Select
+BUTTON CANCEL Cancel
+Creating Function Arg Array
+
+{FormChangeCb}
+{cStr1} 
+<##Number of args      :{iArgCount}>
+
+
+
+""", {      'FormChangeCb': F.FormChangeCb(self.OnFormChange),
+            'cStr1': F.StringLabel("<span style='float:left;'>How many arguments will you be passing?<span>", tp=F.FT_HTML_LABEL),
+            'iArgCount': F.StringInput(),
+        })
+
+        
+    def OnFormChange(self, fid):
+        return 1
+
+    @staticmethod
+    def show():
+        f = arg_num_select_t()
+        f, args = f.Compile()
+        # Show form
+        ok = f.Execute()
+        if ok == 1:
+            arg_count = f.iArgCount.value
+            f.Free()
+            print("Initiated " + arg_count + " args in array")
+            return arg_count
+        else:
+            f.Free()
+            return None
+#--------------------------------------------------------------------------
+# Arg chooser Form
+#--------------------------------------------------------------------------
+class arg_select_t(ida_kernwin.Form):
+    """Simple form to select argument"""
+    def __init__(self):
+        self.__n = 0
+        F = ida_kernwin.Form
+        F.__init__(self,
+r"""BUTTON YES* Select
+BUTTON CANCEL Cancel
+Choose which argument to set
+
+{FormChangeCb}
+{cStr1} 
+<##Argument      :{iArgNum}>
+
+
+
+""", {      'FormChangeCb': F.FormChangeCb(self.OnFormChange),
+            'cStr1': F.StringLabel("<span style='float:left;'>Which argument would you like to set?<span>", tp=F.FT_HTML_LABEL),
+            'iArgNum': F.StringInput(),
+        })
+
+        
+    def OnFormChange(self, fid):
+        return 1
+
+    @staticmethod
+    def show():
+        f = arg_select_t()
+        f, args = f.Compile()
+        # Show form
+        ok = f.Execute()
+        if ok == 1:
+            arg_choice = f.iArgNum.value
+            f.Free()
+            print("Setting argument " + arg_choice)
+            return arg_choice
+        else:
+            f.Free()
+            return None
+#--------------------------------------------------------------------------
+# Dumpulator functions
+#--------------------------------------------------------------------------
+def emulate_func():
+    """
+    Emulate your function
+    """
+
+    idaapi.msg("Emulating function:\n")
+
+
+    dp.call(CALL_ADDRESS, func_args)
+    
+    for arg in func_args:
+        try:
+            print("Address " + str(arg) + ": " + dp.read_str(arg))
+        except:
+            print("Address " + str(arg) + " could not be stringified")
+
+    return True
+
+
+def set_temp_addr_arg():
+    """
+    Set argument to a temp address
+    """
+    argument_selection = (int(arg_select_t.show()) - 1)
+    if ((argument_selection)>len(func_args)):
+        print("Argument " + str(argument_selection) + " doesn't exist...")
+    else:
+        func_args[argument_selection] = dp.allocate(256)
+        print("Allocated temporary space for argument " + str(argument_selection+1) + ". Address set to " + str(hex(func_args[argument_selection])))
+
+    return True
+
+def set_arg():
+    """
+    Set argument to selected address
+    """
+    global first_arg
+    #value = parse_highlighted_value("ERROR: Not a valid value selection\n")
+    #value = copy_bytes_py3()
+    value = idc.get_screen_ea()
+
+    argument_selection = (int(arg_select_t.show()) - 1)
+    if ((argument_selection)>len(func_args)):
+        print("Argument " + str(argument_selection) + " doesn't exist...")
+    else:
+        print("Setting argument " + str(argument_selection+1) + " to " + str(hex(value)))
+        func_args[argument_selection] = value
+
+    return True
+
 def run_single_arg():
     """
     Run with dumpulator single argument
@@ -49,7 +186,7 @@ def run_single_arg():
     idaapi.msg("running with %s as single arg\n" % value)
     print(hex(value))
 
-    dp = Dumpulator(DUMP_FILE)
+    
 
     temp_addr = dp.allocate(256)
 
@@ -75,6 +212,10 @@ def set_call_addr():
 
     idaapi.msg("call address is set: %s\n" % value)
     print(hex(value))
+
+    global func_args
+    func_arg_num = arg_num_select_t.show()
+    func_args = [0] * int(func_arg_num)
     
     return True
 #--------------------------------------------------------------------------
@@ -108,6 +249,9 @@ class dumpulate_Plugin_t(idaapi.plugin_t):
             # initialize the menu actions our plugin will inject
             self._init_action_run_single_arg()
             self._init_action_set_call_addr()
+            self._init_action_set_temp_addr_arg()
+            self._init_action_set_arg()
+            self._init_action_emulate_func()
             # initialize plugin hooks
             self._init_hooks()
             return idaapi.PLUGIN_KEEP
@@ -131,6 +275,9 @@ class dumpulate_Plugin_t(idaapi.plugin_t):
         # unregister our actions & free their resources
         self._del_action_run_single_arg()
         self._del_action_set_call_addr()
+        self._del_action_set_temp_addr_arg()
+        self._del_action_set_arg()
+        self._del_action_emulate_func
         # done
         idaapi.msg("%s terminated...\n" % self.wanted_name)
 
@@ -138,12 +285,15 @@ class dumpulate_Plugin_t(idaapi.plugin_t):
     #--------------------------------------------------------------------------
     # IDA Actions
     #--------------------------------------------------------------------------
-    ACTION_RUN_SINGLE_ARG  = "dumpulate:setarg"
+    ACTION_RUN_SINGLE_ARG  = "dumpulate:runsinglearg"
     ACTION_SET_CALL_ADDR = "dumpulate:setcalladdr"
+    ACTION_SET_TEMP_ADDR_ARG = "dumpulate:settempaddrarg"
+    ACTION_SET_ARG = "dumpulate:setarg"
+    ACTION_EMULATE_FUNC = "dumpulate:emulatefunc"
     
     def _init_action_run_single_arg(self):
         """
-        Register the set arg action with IDA.
+        Register the run single arg action with IDA.
         """
         action_desc = idaapi.action_desc_t(
             self.ACTION_RUN_SINGLE_ARG,         # The action name.
@@ -158,7 +308,7 @@ class dumpulate_Plugin_t(idaapi.plugin_t):
 
     def _init_action_set_call_addr(self):
         """
-        Register the set arg action with IDA.
+        Register the set call address action with IDA.
         """
         action_desc = idaapi.action_desc_t(
             self.ACTION_SET_CALL_ADDR,         # The action name.
@@ -171,11 +321,65 @@ class dumpulate_Plugin_t(idaapi.plugin_t):
         # register the action with IDA
         assert idaapi.register_action(action_desc), "Action registration failed"
 
+    def _init_action_set_temp_addr_arg(self):
+        """
+        Register the set temp addr arg action with IDA.
+        """
+        action_desc = idaapi.action_desc_t(
+            self.ACTION_SET_TEMP_ADDR_ARG,         # The action name.
+            "Dumpulate Allocate Temp For Argument",                     # The action text.
+            IDACtxEntry(set_temp_addr_arg),        # The action handler.
+            None,                  # Optional: action shortcut
+            "Allocate temporary address space for arg"   # Optional: tooltip
+            
+        )
+        # register the action with IDA
+        assert idaapi.register_action(action_desc), "Action registration failed"
+
+    def _init_action_set_arg(self):
+        """
+        Register the set arg action with IDA.
+        """
+        action_desc = idaapi.action_desc_t(
+            self.ACTION_SET_ARG,         # The action name.
+            "Dumpulate Set Argument",                     # The action text.
+            IDACtxEntry(set_arg),        # The action handler.
+            None,                  # Optional: action shortcut
+            "Set arg to selected address"   # Optional: tooltip
+            
+        )
+        # register the action with IDA
+        assert idaapi.register_action(action_desc), "Action registration failed"
+
+    def _init_action_emulate_func(self):
+        """
+        Register the set arg action with IDA.
+        """
+        action_desc = idaapi.action_desc_t(
+            self.ACTION_EMULATE_FUNC,         # The action name.
+            "Dumpulate Emulate Function",                     # The action text.
+            IDACtxEntry(emulate_func),        # The action handler.
+            None,                  # Optional: action shortcut
+            "Emulate your function"   # Optional: tooltip
+            
+        )
+        # register the action with IDA
+        assert idaapi.register_action(action_desc), "Action registration failed"
+
     def _del_action_set_call_addr(self):
         idaapi.unregister_action(self.ACTION_SET_CALL_ADDR)
 
     def _del_action_run_single_arg(self):
         idaapi.unregister_action(self.ACTION_RUN_SINGLE_ARG)
+
+    def _del_action_set_temp_addr_arg(self):
+        idaapi.unregister_action(self.ACTION_SET_TEMP_ADDR_ARG)
+
+    def _del_action_set_arg(self):
+        idaapi.unregister_action(self.ACTION_SET_ARG)
+
+    def _del_action_emulate_func(self):
+        idaapi.unregister_action(self.ACTION_EMULATE_FUNC)
 
     #--------------------------------------------------------------------------
     # Initialize Hooks
@@ -231,7 +435,7 @@ class Hooks(idaapi.UI_Hooks):
                 form,
                 popup,
                 dumpulate_Plugin_t.ACTION_RUN_SINGLE_ARG,
-                "Dumpulate - set arg",
+                "Dumpulate - run single arg",
                 idaapi.SETMENU_APP,
             )
 
@@ -240,6 +444,30 @@ class Hooks(idaapi.UI_Hooks):
                 popup,
                 dumpulate_Plugin_t.ACTION_SET_CALL_ADDR,
                 "Dumpulate - set call address",
+                idaapi.SETMENU_APP,
+            )
+
+            idaapi.attach_action_to_popup(
+                form,
+                popup,
+                dumpulate_Plugin_t.ACTION_SET_TEMP_ADDR_ARG,
+                "Dumpulate - set temp addr arg",
+                idaapi.SETMENU_APP,
+            )
+
+            idaapi.attach_action_to_popup(
+                form,
+                popup,
+                dumpulate_Plugin_t.ACTION_SET_ARG,
+                "Dumpulate - set argument",
+                idaapi.SETMENU_APP,
+            )
+
+            idaapi.attach_action_to_popup(
+                form,
+                popup,
+                dumpulate_Plugin_t.ACTION_EMULATE_FUNC,
+                "Dumpulate - emulate function",
                 idaapi.SETMENU_APP,
             )
 
@@ -266,7 +494,7 @@ def inject_actions(form, popup, form_type):
             form,
             popup,
             dumpulate_Plugin_t.ACTION_RUN_SINGLE_ARG,
-            "Dumpulate set arg",
+            "Dumpulate run with single arg",
             idaapi.SETMENU_APP
         )
 
@@ -275,6 +503,30 @@ def inject_actions(form, popup, form_type):
             popup,
             dumpulate_Plugin_t.ACTION_SET_CALL_ADDR,
             "Dumpulate set call address",
+            idaapi.SETMENU_APP
+        )
+
+        idaapi.attach_action_to_popup(
+            form,
+            popup,
+            dumpulate_Plugin_t.ACTION_SET_TEMP_ADDR_ARG,
+            "Dumpulate set temp addr arg",
+            idaapi.SETMENU_APP
+        )
+
+        idaapi.attach_action_to_popup(
+            form,
+            popup,
+            dumpulate_Plugin_t.ACTION_SET_ARG,
+            "Dumpulate set argument",
+            idaapi.SETMENU_APP
+        )
+
+        idaapi.attach_action_to_popup(
+            form,
+            popup,
+            dumpulate_Plugin_t.ACTION_EMULATE_FUNC,
+            "Dumpulate Emulate Function",
             idaapi.SETMENU_APP
         )
 
